8eeee6f187e6ce8aafc6fbf2e1b3fd068a6d0813
Hacker6329 <Hacker6329@gmail.com>
2023-06-02 03:51:04 +0200

    Refactored code

--- src/main/java/it/italiandudes/myrpgmanager/javafx/scene/SceneCreateOrChooseDB.java
+++ src/main/java/it/italiandudes/myrpgmanager/javafx/scene/SceneCreateOrChooseDB.java
@@ -1,6 +1,7 @@
 package it.italiandudes.myrpgmanager.javafx.scene;
 
 import it.italiandudes.idl.common.Logger;
+import it.italiandudes.myrpgmanager.MyRPGManager;
 import it.italiandudes.myrpgmanager.javafx.JFXDefs;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -12,7 +13,7 @@ public final class SceneCreateOrChooseDB {
     //Scene Generator
     public static Scene getScene(){
         try {
-            return new Scene(FXMLLoader.load(JFXDefs.Resource.get(JFXDefs.Resource.FXML.FXML_CREATE_OR_CHOOSE_DB)));
+            return new Scene(FXMLLoader.load(MyRPGManager.Defs.Resources.get(JFXDefs.Resource.FXML.FXML_CREATE_OR_CHOOSE_DB)));
         }catch (IOException e){
             Logger.log(e);
             return null;
--- src/main/java/it/italiandudes/myrpgmanager/javafx/scene/SceneLoading.java
+++ src/main/java/it/italiandudes/myrpgmanager/javafx/scene/SceneLoading.java
@@ -1,6 +1,7 @@
 package it.italiandudes.myrpgmanager.javafx.scene;
 
 import it.italiandudes.idl.common.Logger;
+import it.italiandudes.myrpgmanager.MyRPGManager;
 import it.italiandudes.myrpgmanager.javafx.JFXDefs;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -13,7 +14,7 @@ public final class SceneLoading {
     //Scene Generator
     public static Scene getScene(){
         try {
-            return new Scene(FXMLLoader.load(JFXDefs.Resource.get(JFXDefs.Resource.FXML.FXML_LOADING)));
+            return new Scene(FXMLLoader.load(MyRPGManager.Defs.Resources.get(JFXDefs.Resource.FXML.FXML_LOADING)));
         }catch (IOException e){
             Logger.log(e);
             return null;
