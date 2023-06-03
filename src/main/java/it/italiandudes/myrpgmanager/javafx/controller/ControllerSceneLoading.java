ae3fa1f907bc66e17204f54aa0f5618d1917b91c
Hacker6329 <hacker6329@gmail.com>
2023-06-03 08:42:43 +0200

    Optimized SceneLoading

--- src/main/java/it/italiandudes/myrpgmanager/javafx/controller/ControllerSceneLoading.java
+++ src/main/java/it/italiandudes/myrpgmanager/javafx/controller/ControllerSceneLoading.java
@@ -1,29 +1,14 @@
 package it.italiandudes.myrpgmanager.javafx.controller;
 
-import it.italiandudes.myrpgmanager.MyRPGManager;
 import it.italiandudes.myrpgmanager.javafx.Client;
-import it.italiandudes.myrpgmanager.javafx.JFXDefs;
 import javafx.fxml.FXML;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import org.jetbrains.annotations.NotNull;
 
 @SuppressWarnings("unused")
 public final class ControllerSceneLoading {
-
-    //Attributes
-    @NotNull
-    private static final Image GIF_LOADING = new Image(MyRPGManager.Defs.Resources.get(JFXDefs.Resource.GIF.GIF_LOADING).toString());
-
-    //FXML Elements
-    @FXML private ImageView loadingView;
-
 
     //Initialize
     @FXML
     private void initialize() {
         Client.getStage().setResizable(false);
-        loadingView.setImage(GIF_LOADING);
     }
-
 }
