15debd991c5ee599cf97603f3c3c068eff902f4a
Hacker6329 <hacker6329@gmail.com>
2023-06-03 11:23:15 +0200

    Updated dnd5e.sql

--- src/main/resources/sql/dnd5e.sql
+++ src/main/resources/sql/dnd5e.sql
@@ -1,3 +1,4 @@
+-- TABLES DECLARATION
 CREATE TABLE IF NOT EXISTS languages (
     id INTEGER NOT NULL PRIMARY KEY,
     name INTEGER NOT NULL UNIQUE,
@@ -110,14 +111,6 @@ CREATE TABLE IF NOT EXISTS spell_components (
     description TEXT
 );
 
-CREATE TABLE IF NOT EXISTS equipment_packs (
-    id INTEGER NOT NULL PRIMARY KEY,
-    name TEXT NOT NULL UNIQUE,
-    rarity TEXT NOT NULL DEFAULT 0,
-    cost_copper INTEGER NOT NULL DEFAULT 0,
-    content TEXT NOT NULL
-);
-
 CREATE TABLE IF NOT EXISTS items (
     id INTEGER NOT NULL PRIMARY KEY,
     base64image TEXT,
@@ -128,18 +121,17 @@ CREATE TABLE IF NOT EXISTS items (
     weight REAL
 );
 
-CREATE TABLE IF NOT EXISTS item_tags (
-    id INTEGER NOT NULL PRIMARY KEY,
-    item_id INTEGER NOT NULL REFERENCES items(id),
-    tag TEXT NOT NULL,
-    UNIQUE (item_id, tag)
-);
-
-CREATE TABLE IF NOT EXISTS spell (
+CREATE TABLE IF NOT EXISTS equipment_packs (
     id INTEGER NOT NULL PRIMARY KEY,
     item_id INTEGER NOT NULL UNIQUE REFERENCES items(id),
-    min_level INTEGER DEFAULT 0,
-    spell_type TEXT,
+    content TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS spells (
+    id INTEGER NOT NULL PRIMARY KEY,
+    item_id INTEGER NOT NULL UNIQUE REFERENCES items(id),
+    level INTEGER DEFAULT 0,
+    type TEXT,
     cast_time TEXT,
     spell_range REAL,
     components TEXT,
@@ -163,9 +155,9 @@ CREATE TABLE IF NOT EXISTS properties (
 
 CREATE TABLE IF NOT EXISTS armors (
     id INTEGER NOT NULL PRIMARY KEY,
-    item_id INTEGER NOT NULL UNIQUE,
+    item_id INTEGER NOT NULL UNIQUE REFERENCES items(id),
     category TEXT,
-    ac TEXT,
+    ac INTEGER,
     strength_required INTEGER,
     stealth TEXT
 );
@@ -177,3 +169,77 @@ CREATE TABLE IF NOT EXISTS talents (
     description TEXT,
     privileges TEXT
 );
+
+-- VIEWS DECLARATION
+CREATE VIEW IF NOT EXISTS v_items AS
+    SELECT
+        i.id AS ItemID,
+        i.name AS Name,
+        i.base64image AS Base64Image,
+        i.cost_copper AS CostCopper,
+        i.description AS Description,
+        i.rarity AS Rarity,
+        i.weight AS Weight
+    FROM items AS i
+;
+CREATE VIEW IF NOT EXISTS v_armors AS
+    SELECT
+        i.id AS ItemID,
+        a.id AS ArmorID,
+        i.name AS Name,
+        i.base64image AS Base64Image,
+        i.cost_copper AS CostCopper,
+        i.description AS Description,
+        i.rarity AS Rarity,
+        i.weight AS Weight,
+        a.category AS Category,
+        a.ac AS AC,
+        a.strength_required AS StrengthRequired,
+        a.stealth AS Stealth
+    FROM
+        items AS i
+        JOIN armors AS a
+        ON i.id = a.item_id
+;
+
+CREATE VIEW IF NOT EXISTS v_weapons AS
+    SELECT
+        i.id AS ItemID,
+        w.id AS WeaponID,
+        i.name AS Name,
+        i.base64image AS Base64Image,
+        i.cost_copper AS CostCopper,
+        i.description AS Description,
+        i.rarity AS Rarity,
+        i.weight AS Weight,
+        w.category AS Category,
+        w.damage AS Damage,
+        w.properties AS Properties
+    FROM
+        items AS i
+        JOIN weapons AS w
+        ON i.id = w.item_id
+;
+
+CREATE VIEW IF NOT EXISTS v_spells AS
+    SELECT
+        i.id AS ItemID,
+        s.id AS SpellID,
+        i.name AS Name,
+        i.base64image AS Base64Image,
+        i.cost_copper AS CostCopper,
+        i.description AS Description,
+        i.rarity AS Rarity,
+        i.weight AS Weight,
+        s.level AS Level,
+        s.type AS Type,
+        s.cast_time AS CastTime,
+        s.spell_range AS Range,
+        s.components AS Components,
+        s.duration AS Duration,
+        s.damage AS Damage
+    FROM
+        items AS i
+        JOIN spells AS s
+        ON i.id = s.item_id
+;
