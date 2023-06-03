c5292bcd116ca8e81103bcc0239d6041ecaa0d48
Hacker6329 <hacker6329@gmail.com>
2023-06-03 11:25:02 +0200

    Started implementation of ControllerSceneDND5EMainMenu

---
+++ src/main/java/it/italiandudes/myrpgmanager/javafx/controller/dnd5e/ControllerSceneDND5EMainMenu.java
@@ -0,0 +1,36 @@
+package it.italiandudes.myrpgmanager.javafx.controller.dnd5e;
+
+import it.italiandudes.myrpgmanager.javafx.Client;
+import javafx.fxml.FXML;
+
+public final class ControllerSceneDND5EMainMenu {
+
+    //Initialize
+    @FXML
+    private void initialize() {
+        Client.getStage().setResizable(true);
+    }
+
+    // EDT
+    @FXML
+    private void openRaces() {
+
+    }
+    @FXML
+    private void openClasses() {
+
+    }
+    @FXML
+    private void openItems() {
+
+    }
+    @FXML
+    private void openTalents() {
+
+    }
+    @FXML
+    private void openBackgrounds() {
+
+    }
+
+}
