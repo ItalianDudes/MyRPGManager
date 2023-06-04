9c8ab719dfd11a8144004a708f5c32511caf7bbf
Hacker6329 <Hacker6329@gmail.com>
2023-06-05 01:17:17 +0200

    Started implementation of ControllerSceneDND5EItem

--- src/main/java/it/italiandudes/myrpgmanager/javafx/controller/dnd5e/ControllerSceneDND5EItem.java
+++ src/main/java/it/italiandudes/myrpgmanager/javafx/controller/dnd5e/ControllerSceneDND5EItem.java
@@ -2,19 +2,36 @@ package it.italiandudes.myrpgmanager.javafx.controller.dnd5e;
 
 import it.italiandudes.myrpgmanager.javafx.Client;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 
 @SuppressWarnings("unused")
 public final class ControllerSceneDND5EItem {
 
-    // Attributes
-
     // Graphic Elements
+    @FXML private TextField textFieldName;
+    @FXML private TextField textFieldWeight;
+    @FXML private ComboBox<String> comboBoxRarity;
+    @FXML private TextField textFieldMR;
+    @FXML private TextField textFieldMA;
+    @FXML private TextField textFieldME;
+    @FXML private TextField textFieldMO;
+    @FXML private TextField textFieldMP;
+    @FXML private TextArea textAreaDescription;
+    @FXML private ImageView imageViewItem;
 
     // Initialize
     @FXML
     private void initialize() {
         Client.getStage().setResizable(true);
+        // TODO: initialize()
     }
 
     // EDT
+    @FXML
+    private void save() {
+        // TODO: save()
+    }
 }
